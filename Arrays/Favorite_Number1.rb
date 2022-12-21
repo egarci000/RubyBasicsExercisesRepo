9cb400fc8898402c7a8892ed3fa4f0158ca19f89
egarci000 <115583629+egarci000@users.noreply.github.com>
2022-12-21 15:36:11 -0600

    completed the last three exercises in the Arrays section of Ruby Basics

---
+++ Arrays/Favorite_Number1.rb
@@ -0,0 +1,13 @@
+array = ['Dave', 7, 'Miranda', 3, 'Jason', 11]
+array = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+
+array_dave = ['Dave', 7]
+array_miranda = ['Miranda', 3]
+array_jason = ['Jason', 11]
+array_of_three = []
+
+array_of_three << array_dave
+array_of_three << array_miranda
+array_of_three << array_jason
+
+p array_of_three
