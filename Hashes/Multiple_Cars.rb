76827a96f376a68e70eca604f0798b05ca7ee6d3
egarci000 <115583629+egarci000@users.noreply.github.com>
2022-12-26 17:26:54 -0600

    completed Hashes section in Ruby Basics

---
+++ Hashes/Multiple_Cars.rb
@@ -0,0 +1,33 @@
+# vehicles = {
+#   {
+#     type: 'sedan',
+#     color: 'blue',
+#     year: 2003
+#   },
+#   {
+#     type: 'pickup',
+#     color: 'red',
+#     year: 1998
+#   }
+# }
+
+
+vehicles = {
+  car:{
+    type: 'sedan', 
+    color: 'blue', 
+    year: 2003
+  }, 
+  truck:{
+    type: 'pickup', 
+    color: 'red',
+    year:1998
+  }
+}
+
+vehicles = {
+  car:  {  type: 'sedan', color: 'blue', year: 2003}, 
+  truck:  { type: 'pickup', color: 'red', year:1998}
+}
+
+p vehicles
